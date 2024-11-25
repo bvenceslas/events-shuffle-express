68fa67444f48d589f707617c299031ea518ee645
bvenceslas <bvenceslas@gmail.com>
2024-11-25 17:30:44 +0200

    feat (api) added versionning

--- src/participant/participant.controller.ts
+++ src/participant/participant.controller.ts
@@ -2,32 +2,35 @@ import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ParticipantService } from './participant.service';
 import { ParticipantDto } from './dto/participant.dto';
 
-@Controller('participant')
+@Controller({
+  path: 'participant',
+  version: '1',
+})
 export class ParticipantController {
   constructor(private readonly participantService: ParticipantService) {}
 
   @Post()
-  async create(@Body() createParticipantDto: ParticipantDto) {
+  async createV1(@Body() createParticipantDto: ParticipantDto) {
     return await this.participantService.create(createParticipantDto);
   }
 
   @Get()
-  async getAllParticipants() {
+  async getAllParticipantsV1() {
     return await this.participantService.findAll();
   }
 
   @Get(':id')
-  async getParticipantById(@Param('id') participantId: string) {
+  async getParticipantByIdV1(@Param('id') participantId: string) {
     return await this.participantService.findOneById(participantId);
   }
 
   @Get('/name/:name')
-  async getParticipantByname(@Param('name') participantName: string) {
-    return await this.participantService.findOneById(participantName);
+  async getParticipantByNameV1(@Param('name') participantName: string) {
+    return await this.participantService.findOneByName(participantName);
   }
 
   @Put(':id')
-  async update(
+  async updateV1(
     @Param('id') participantId: string,
     @Body() updateParticipantDto: ParticipantDto,
   ) {
